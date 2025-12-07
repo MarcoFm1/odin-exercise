b2460c26e3c86b5d79cd63cc2a6702a868ef097b
marco fm <marco.fini22@gmail.com>
2025-12-06 23:42:16 -0300

    ej

--- op-ejercicios/others/array.js
+++ op-ejercicios/others/array.js
@@ -56,4 +56,59 @@ alert( arr4.indexOf(null) ); // -1
 
 alert( arr4.includes(1) ); // true
 
+//===================================================== find : If it returns true, the search is stopped, the item is returned. If nothing is found, undefined is returned.
+
+let users = [
+  {id: 1, name: "John"},
+  {id: 2, name: "Pete"},
+  {id: 3, name: "Mary"}
+];
+
+let user = users.find(item => item.id == 1);
+
+alert(user.name); // John
+
+//===================================================== filter : returns an array of all matching elements
+
+let users2 = [
+  {id: 1, name: "John"},
+  {id: 2, name: "Pete"},
+  {id: 3, name: "Mary"}
+];
+
+// returns array of the first two users
+let someUsers = users2.filter(item => item.id < 3);
+
+alert(someUsers.length); // 2
+
+//===================================================== map : It calls the function for each element of the array and returns the array of results
+
+let lengths = ["Bilbo", "Gandalf", "Nazgul"].map(item => item.length);
+alert(lengths);
+
+//===================================================== split : It splits the string into an array by the given delimiter delim
+let names = 'Bilbo, Gandalf, Nazgul';
+
+let arr5 = names.split(', '); // .split(', ', 2) -> Bilbo, Gandalf
+
+for (let name of arr5) {
+  alert( `A message to ${name}.` ); // A message to Bilbo  (and other names)
+}
+
+//===================================================== join : does the reverse to split. It creates a string of arr items joined by glue between them.
+
+let arr6 = ['Bilbo', 'Gandalf', 'Nazgul'];
+
+let str = arr6.join(';'); // glue the array into a string using ;
+
+alert( str ); // Bilbo;Gandalf;Nazgul
+
+//===================================================== reduce : It are used to calculate a single value based on the array (ver)
+
+let arr7 = [1, 2, 3, 4, 5];
+
+let result = arr7.reduce((sum, current) => sum + current, 0); // sintaxis : let value = arr.reduce(function(accumulator, item, index, array) {
+
+alert(result); // 15
+
 
