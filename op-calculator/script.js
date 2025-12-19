61e6ca20aed05f8aa85e6a60a3c90c88da57b910
marco fm <marco.fini22@gmail.com>
2025-12-18 23:57:08 -0300

    a

--- op-calculator/script.js
+++ op-calculator/script.js
@@ -1,68 +1,19 @@
-const btnOne = document.getElementById("btn-1");
-const btnTwo = document.getElementById("btn-2");
-const btnThree = document.getElementById("btn-3");
-const btnFour = document.getElementById("btn-4");
-const btnFive = document.getElementById("btn-5");
-const btnSix = document.getElementById("btn-6");
-const btnSeven = document.getElementById("btn-7");
-const btnEight = document.getElementById("btn-8");
-const btnNine = document.getElementById("btn-9");
-const btnZero = document.getElementById("btn-0");
+const displayBox = document.querySelector(".div-screen");
+const displayInput = document.querySelector(".display-input");
+const displayResult = document.querySelector(".display-result");
+const buttons = document.querySelectorAll("button");
+const operators = ["%","/","x","-","+"];
 
-let currentNumber = "";
-const screen = document.getElementById("screen-text");
+let input = "";
+let result = "";
 
+function calculate(btnValue){
+    input += btnValue;
 
-
-function getNumber(){
-    btnOne.addEventListener("click", () => printNumber(1));
-    btnTwo.addEventListener("click", () => printNumber(2));
-    btnThree.addEventListener("click", () => printNumber(3));
-    btnFour.addEventListener("click", () => printNumber(4));
-    btnFive.addEventListener("click", () => printNumber(5));
-    btnSix.addEventListener("click", () => printNumber(6));
-    btnSeven.addEventListener("click", () => printNumber(7));
-    btnEight.addEventListener("click", () => printNumber(8));
-    btnNine.addEventListener("click", () => printNumber(9));
-    btnZero.addEventListener("click", () => printNumber(0));
+    displayInput.value = input;
+    displayResult.value = result;
 }
 
-getNumber();
-
-function printNumber(num){
-    currentNumber += num;   
-    screen.innerText = currentNumber; 
-    console.log(currentNumber)
-}
-
-function plusFunction(){
-    let num1 = currentNumber;
-    let num2 = printNumber();
-    if(num1) {
-        
-    }
-}
-
-function restFunction(){
-    
-}
-
-function divFunction(){
-    
-}
-
-function multFunction(){
-    
-}
-
-function OnFunction(){
-    
-}
-
-function ACFunction(){
-
-}
-
-function C(){
-
-}
+buttons.forEach(button => {
+    button.addEventListener("click", e => calculate(e.target.textContent))
+})
