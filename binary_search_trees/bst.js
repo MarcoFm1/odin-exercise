c165fccffb21a88c38ecf2a4817dd854eca0a19a
marco fm <marco.fini22@gmail.com>
2026-02-17 23:01:50 -0300

    binary search

---
+++ binary_search_trees/bst.js
@@ -0,0 +1,51 @@
+class Node{
+    constructor(data, left, right){
+        this._data = data
+        this._left = left
+        this._right = right
+    }
+}
+
+class Tree{
+    constructor(root){
+        this._root = root
+    }
+
+    buildTree(arr){
+        
+    }
+
+    includes(value){
+
+    }
+
+    deleteItem(value){
+
+    }
+
+    levelOrderForEach(callback){
+
+    }
+
+    inOrderForEach(callback){
+
+    }
+    preOrderForEach(callback){
+
+    }
+    postOrderForEach(callback){
+
+    } 
+
+    height(value){
+
+    }
+
+    depth(value){
+        
+    }
+
+    isBalanced(){
+
+    }
+}
