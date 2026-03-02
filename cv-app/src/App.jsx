a4c6b346c8f4ab1c15cc4409904f31fb6ead686e
marco fm <marco.fini22@gmail.com>
2026-03-02 16:53:04 -0300

    cv start

---
+++ cv-app/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind is working 🚀
+      </h1>
+       <Button>Click me</Button>
+
+    </>
+  )
+}
+
+export default App
